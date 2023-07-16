f4dc4743780f49026ee7544e043e6ea8111f90d1
LSnumber1 <1347966554@qq.com>
2023-07-16 22:02:15 +0800

    [Feat] 添加障碍物

--- ember_quest_demo/lib/ember_quest.dart
+++ ember_quest_demo/lib/ember_quest.dart
@@ -1,13 +1,20 @@
 import 'dart:async';
 
 import 'package:ember_quest_demo/actors/ember.dart';
+import 'package:ember_quest_demo/managers/segment_manager.dart';
 import 'package:flame/components.dart';
 import 'package:flame/game.dart';
+
+import 'actors/water_enemy.dart';
+import 'objects/ground_block.dart';
+import 'objects/platform_block.dart';
+import 'objects/star.dart';
 
 class EmberQuestGame extends FlameGame {
   EmberQuestGame();
 
   late EmberPlayer _ember;
+  double objectSpeed = 0.0;
   final world = World();
   late final CameraComponent cameraComponent;
 
@@ -27,7 +34,38 @@ class EmberQuestGame extends FlameGame {
     cameraComponent.viewfinder.anchor = Anchor.topLeft;
     addAll([cameraComponent, world]);
 
+    initializeGame();
+  }
+
+  void initializeGame() {
+    final segmentsToLoad = (size.x / 640).ceil();
+    segmentsToLoad.clamp(0, segments.length);
+
+    for (var i = 0; i <= segmentsToLoad; i++) {
+      loadGameSegments(i, (i * 640).toDouble());
+    }
+
     _ember = EmberPlayer(position: Vector2(128, canvasSize.y - 70));
     world.add(_ember);
+  }
+
+  void loadGameSegments(int segmentIndex, double xPositionOffset) {
+    for (final block in segments[segmentIndex]) {
+      switch (block.blockType) {
+        case GroundBlock:
+          break;
+        case PlatformBlock:
+          add(PlatformBlock(
+            gridPosition: block.gridPosition,
+            xOffset: xPositionOffset,
+          ));
+          break;
+        case Star:
+          break;
+        case WaterEnemy:
+          break;
+        default:
+      }
+    }
   }
 }
